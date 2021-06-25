ce77f45894299ef50c303f5e43eb649fd10f1933
womyn <1006102877@qq.com>
2021-06-25 17:46:21 +0800

    second commit

---
+++ src/main/java/com/xuexiangban/rabbitmq/direct/Producer.java
@@ -0,0 +1,67 @@
+package com.xuexiangban.rabbitmq.direct;
+
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Producer {
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.130.140");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("admin");
+        connectionFactory.setPassword("admin");
+        connectionFactory.setVirtualHost("/");
+
+
+        Connection connection = null;
+        Channel channel = null;
+        try {
+
+            connection = connectionFactory.newConnection("生产者");
+
+            channel = connection.createChannel();
+
+            String message = "Hello xuexiangban!!!";
+            String exchangeName="fanout-exchange";
+
+            String routekey="";
+            String type="fanout";
+
+            channel.basicPublish(exchangeName, routekey, null, message.getBytes());
+            System.out.println("消息发送成功");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+
+            if (channel != null && channel.isOpen()) {
+
+                try {
+                    channel.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+
+                if (connection != null && connection.isOpen()) {
+
+                    try {
+                        connection.close();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+
+
+                }
+
+
+            }
+
+
+        }
+    }
+}
